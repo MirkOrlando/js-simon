f7a67a0c4c488513830316c20ff5e6390ffe0c22
MirkOrlando <mirko@orlando-m.it>
2022-04-06 14:24:08 +0200

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* Visualizzare in pagina 5 numeri casuali. 
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, 
+i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, 
+il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+/* calcolare i numeri casuali */
+
+/* visualizzare i numeri casuali (dopo 30 secondi scompaiono) */
+
+/* l'utente inserisce i numeri che ha visualizzato in precedenza */
+
+/* verificare quanti e quali numeri l'utente riscordava */
+
+/* visualizzare il risultato */
