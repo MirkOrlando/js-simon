74fee5e853510113e13529c65117bbe0b61e531b
MirkOrlando <mirko@orlando-m.it>
2022-04-06 15:33:42 +0200

    working on function to set hidden

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,45 @@ il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
 /* calcolare i numeri casuali */
-
+const numbers = randomNumbers(5);
 /* visualizzare i numeri casuali (dopo 30 secondi scompaiono) */
-
+// setTimeout(printArraySplitted, 3000);
+printArraySplitted(numbers, "div", "number_container");
+// aggiungo style "hidden"
+addStyle(".number_container");
 /* l'utente inserisce i numeri che ha visualizzato in precedenza */
 
 /* verificare quanti e quali numeri l'utente riscordava */
 
 /* visualizzare il risultato */
+
+//functions
+function randomNumbers(n) {
+  const numbers = [];
+  while (numbers.length < n) {
+    const rndnumber = Math.ceil(Math.random() * 100);
+    console.log(rndnumber);
+    numbers.push(rndnumber);
+  }
+  console.log(numbers);
+  return numbers;
+}
+
+function printArraySplitted(array, tagName, className) {
+  // stampo ogni numero in un contenitore
+  for (let i = 0; i < array.length; i++) {
+    const arrayElement = array[i];
+    const containerElement = document.querySelector(".container");
+    const newElement = `<${tagName} class="${className}">${arrayElement}</${tagName}>`;
+    // console.log(newElement);
+    containerElement.insertAdjacentHTML("afterbegin", newElement);
+    // const newElement = document.querySelectorAll(".array_element");
+    // console.log(newElement);
+    /* newElement.innerHTML = arrayElement; */
+  }
+}
+
+function addStyle(selector) {
+  const elements = document.querySelectorAll(selector);
+  console.log(elements);
+}
