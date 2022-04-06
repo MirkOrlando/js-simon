beeff06539762c6e2e9004d2f589c92d7270d664
MirkOrlando <mirko@orlando-m.it>
2022-04-06 17:21:26 +0200

    created Simon Says

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,25 +9,44 @@ il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 /* calcolare i numeri casuali */
 const numbers = createRandomNumbers(5);
 /* visualizzare i numeri casuali (dopo 30 secondi scompaiono) */
-const timeToDisappear = setTimeout(addInvisibility, 5000);
+const timeToDisappear = setTimeout(addInvisibility, 30000);
 printArraySplitted(numbers, "div", "number_container");
 /* l'utente inserisce i numeri che ha visualizzato in precedenza e li stampo a video */
-const askNumbers = setTimeout(typeAndPrintUserNumbers, 5500);
+const askNumbers = setTimeout(typeAndPrintUserNumbers, 30500);
 /* verificare quanti e quali numeri l'utente ricordava */
 /* visualizzare il risultato */
-const gameOver = setTimeout(readResults, 6000);
+const gameOver = setTimeout(readResults, 31000);
 
 //functions
 function readResults() {
   const rndNumbers = document.querySelectorAll(".number_container");
   const userNumbers = document.querySelectorAll(".user_number");
+  const firstRndNumber = document.querySelector(".number_container");
+  const firstUserNumber = document.querySelector(".user_number");
+  let totalPoints = 0;
+  firstRndNumber.insertAdjacentHTML("beforebegin", `<h1>Random Numbers</h1>`);
+  firstUserNumber.insertAdjacentHTML("beforebegin", `<h1>User Numbers</h1>`);
+  const containerElement = document.querySelector(".container");
   for (let i = 0; i < rndNumbers.length; i++) {
     const rndNumber = rndNumbers[i];
-    element.classList.remove("invisible");
+    rndNumber.classList.remove("invisible");
     for (let i = 0; i < userNumbers.length; i++) {
       const userNumber = userNumbers[i];
+      if (userNumber.innerHTML === rndNumber.innerHTML) {
+        userNumber.classList.add("checked");
+        rndNumber.classList.add("checked");
+        ++totalPoints;
+      }
     }
   }
+  setTimeout(FuncAlert, 500);
+  function FuncAlert() {
+    alert(`Punteggio: ${totalPoints}/5`);
+  }
+  containerElement.insertAdjacentHTML(
+    "beforeend",
+    `<h3>Punteggio: ${totalPoints}/5</h3>`
+  );
 }
 
 function typeAndPrintUserNumbers() {
@@ -78,7 +97,9 @@ function createRandomNumbers(n) {
   while (numbers.length < n) {
     const rndnumber = Math.ceil(Math.random() * 100);
     // console.log(rndnumber);
-    numbers.push(rndnumber);
+    if (!numbers.includes(rndnumber)) {
+      numbers.push(rndnumber);
+    }
   }
   // console.log(numbers);
   return numbers;
